27c798fab2f9b3b7b722adc793f5737ed09efff8
Logesh <Logesh@DESKTOP-GTM73SS>
2020-05-04 20:36:42 +0530

    BRanch commit

--- src/test/java/TestGit1/TestGit1/TestGit1.java
+++ src/test/java/TestGit1/TestGit1/TestGit1.java
@@ -4,7 +4,7 @@ public class TestGit1 {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-System.out.println("Testing GIT1");
+System.out.println("Testing GIT1 Tester");
 	}
 
 }
