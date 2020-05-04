5f30fccb38a64b5c5754f43c553595fb77a707a7
logesh.kumar <logesh.kumar@CDC2-L-9LBY5M2.dir.svc.accenture.com>
2020-05-04 19:45:57 +0530

    First Commit

---
+++ src/test/java/TestGit1/TestGit1/TestGit1.java
@@ -0,0 +1,10 @@
+package TestGit1.TestGit1;
+
+public class TestGit1 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+System.out.println("Testing GIT1");
+	}
+
+}
